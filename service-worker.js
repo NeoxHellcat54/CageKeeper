eb9a5667f9c6b17d6d10ff786212b7d1938051ea
NeoxHellcat54 <colejoseph677@gmail.com>
2026-06-30 23:37:00 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+{
+  "name": "CageKeeper",
+  "short_name": "CageKeeper",
+  "description": "Self-lock vault PWA",
+  "start_url": "./index.html?v=0.2.2",
+  "display": "standalone",
+  "background_color": "#030305",
+  "theme_color": "#030305",
+  "orientation": "portrait-primary",
+  "icons": [
+    {
+      "src": "assets/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "assets/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ],
+  "scope": "./"
+}
